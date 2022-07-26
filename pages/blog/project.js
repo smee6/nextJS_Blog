8bfad28f8a65cff3644b0f4e3ca1465f6d804c9d
seomyungin <seo@bitstar.store>
2022-07-26 14:17:01 +0900

    노션 모듈 삭제

--- pages/blog/project.js
+++ pages/blog/project.js
@@ -4,7 +4,7 @@ export default function Project() {
     return (
         <div className={styles.container}>
             <main className={styles.main}>
-                하하하 프로젝트
+                하하하 프로젝트 나눔고딕 Coding 동대입구
             </main>
         </div>
     )
