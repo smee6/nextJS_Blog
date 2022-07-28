3415be55595251826f4d6dd9be1115c13e89ab64
seomyungin <seo@bitstar.store>
2022-07-28 13:34:14 +0900

    url 변경

--- utility/url.js
+++ utility/url.js
@@ -2,8 +2,8 @@ const url = [
     { title: 'Post', link: '/' },
     { title: 'Project', link: '/blog/project' },
     { title: 'About', link: '/blog/about' },
-    { title: 'Guest book', link: '/blog/guestbook' },
     { title: 'Resume', link: '/blog/resume' },
+    { title: 'Guest book', link: '/blog/guestbook' },
 ]
 
 export default url;
